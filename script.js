12147c9b7d6f1ae7bc1f91a145cbca107c225319
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-09-17 09:56:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
 
-function Employee(name, age, jobTitle) {}
+	function greet(){
+		console.log("Hello, my name is ",this.name," I am ",this.age," years old.")
+	}
+}
+
+function Employee(name, age, jobTitle) {
+	constructor(name,age,jobTitle){
+		this.name = name;
+		this.age = age;
+		this.jobTitle = jobTitle;
+	}
+
+	function jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
+
+	__proto__ : Person;
+}
 
 // Do not change code below this line
 window.Person = Person;
